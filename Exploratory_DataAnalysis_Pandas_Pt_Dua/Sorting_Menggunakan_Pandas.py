a7da31f57c1b6af098bd3d149dbd8ec27e7d5e14
Ryan <ryanrizkyf@gmail.com>
2021-02-15 16:08:31 +0700

    edit commit

---
+++ Exploratory_DataAnalysis_Pandas_Pt_Dua/Sorting_Menggunakan_Pandas.py
@@ -0,0 +1,21 @@
+# Sorting adalah sebuah metode mengurutkan data berdasarkan syarat kolom tertentu,
+# dan biasanya digunakan untuk melihat nilai maksimum dan minimum dari dataset.
+# Library Pandas sendiri menyediakan fungsi sorting sebagai fundamental dari exploratory data analysis.
+# Syntax
+# nama_dataframe.sort_values(by="nama_kolom")
+# Contoh
+nilai_skor_df.sort_values(by="Age")
+
+# Function tersebut akan secara default mengurutkan secara ascending (dimulai dari nilai terkecil)
+# Untuk dapat mengurutkan secara descending (nilai terbesar lebih dahulu), dapat menggunakan properti tambahan:
+# Syntax
+# nama_dataframe.sort_values(by="nama_kolom", ascending=False)
+# Contoh
+nilai_skor_df.sort_values(by="Age", ascending=False)
+
+# Fungsi sorting di Pandas juga dapat dilakukan menggunakan lebih dari satu kolom sebagai syarat.
+# Contoh nya pada skenario dibawah, akan mencoba mengaplikasikan fungsi Sorting menggunakan Age dan Score sekaligus:
+# Syntax
+# nama_datafrmae.sort_values(by=["nama_kolom_1", "nama_kolom_2"], ascending=[False, True])
+# Contoh
+nilai_skor_df.sort_values(by=["Age", "Score"], ascending=[True, False])
---
+++ Exploratory_DataAnalysis_Pandas_Pt_Dua/TugasPraktek_Sorting_Menggunakan_Pandas.py
@@ -0,0 +1,8 @@
+import pandas as pd
+order_df = pd.read_csv(
+    "order.csv")
+# Hitung harga maksimum pembelian customer
+sort_harga = order_df.sort_values(by="price", ascending=False)
+print(sort_harga)
+
+# read_csv = https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/order.csv #
