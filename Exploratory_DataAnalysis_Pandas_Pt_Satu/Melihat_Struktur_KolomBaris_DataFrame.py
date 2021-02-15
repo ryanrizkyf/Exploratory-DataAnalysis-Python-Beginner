ea473748eb12c71f4d6b268b6279170613bc6d1a
Ryan <ryanrizkyf@gmail.com>
2021-02-15 10:05:52 +0700

    initialization commit

---
+++ Exploratory_DataAnalysis_Pandas_Pt_Satu/Melihat_Struktur_KolomBaris_DataFrame.py
@@ -0,0 +1,7 @@
+import pandas as pd
+order_df = pd.read_csv(
+    "order.csv")
+print(order_df.shape)
+
+
+# read_csv = https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/order.csv #
---
+++ Exploratory_DataAnalysis_Pandas_Pt_Satu/Membaca_File_CSV_Excel.py
@@ -0,0 +1,8 @@
+# Membaca File CSV
+# [nama_variabel] = pd.read_csv("nama_file.csv")
+
+# Membaca File Excel
+# [nama_variabel] = pd.read_excel("nama_file.xlsx")
+
+import pandas as pd
+order_df = pd.read_csv("order.csv")
