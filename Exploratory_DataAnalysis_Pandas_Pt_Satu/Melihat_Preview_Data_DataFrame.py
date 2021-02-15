63eb959c6a8f3659bf198413ad108461bd0ce78f
Ryan <ryanrizkyf@gmail.com>
2021-02-15 10:20:03 +0700

    edit commit

---
+++ Exploratory_DataAnalysis_Pandas_Pt_Satu/Melihat_Preview_Data_DataFrame.py
@@ -0,0 +1,22 @@
+# Selanjutnya, untuk mendapatkan gambaran dari konten dataframe tersebut.
+# Kita dapat menggunakan function head dan tail, dengan syntax:
+
+# Menampilkan konten teratas dari [nama_dataframe]
+# untuk sejumlah bilangan bulat [jumlah_data]
+# print([nama_dataframe].head([jumlah_data]))
+
+# Jika [jumlah_data] pada function head dan tail dikosongkan maka
+# secara default akan di ditampilkan sebanyak 5 (lima) baris saja.
+# Sehingga bisa ditulis sebagai berikut:
+# DEFAULT: Menampilkan 5 (baris) konten teratas dari [nama_dataframe]
+# print([nama_dataframe].head())
+
+# DEFAULT: Menampilkan 5 (baris) konten terbawah dari [nama_dataframe]
+# print([nama_dataframe].tail())
+
+import pandas as pd
+order_df = pd.read_csv(
+    "order.csv")
+print(order_df.head(10))
+
+# read_csv = https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/order.csv #
---
+++ Exploratory_DataAnalysis_Pandas_Pt_Satu/Stastik_Deskriptif_DataFrame_Pt_Satu.py
@@ -0,0 +1,18 @@
+# Statistik deskriptif atau summary dalam Python - Pandas,
+# dapat diperoleh dengan menggunakan fungsi describe(), yaitu:
+# print([nama_dataframe].describe())
+
+# Function describe dapat memberikan informasi mengenai nilai rataan,
+# standar deviasi dan IQR (interquartile range).
+
+# Ketentuan umum:
+# Secara umum function describe() akan secara otomatis mengabaikan kolom
+# category dan hanya memberikan summary statistik untuk kolom berjenis numerik.
+# Kita perlu menambahkan argument bernama include = "all" untuk mendapatkan
+# summary statistik atau statistik deskriptif dari kolom numerik dan karakter.
+# yaitu
+# print([nama_dataframe].describe(include="all"))
+
+import pandas as pd
+order_df = pd.read_csv('order.csv')
+print(order_df.describe())
